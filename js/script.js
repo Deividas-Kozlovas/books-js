e7cff1ca2adb4fc5d5dd750c01fd695a61e879ea
Deividas Kozlovas <kozlovas.deividas@gmail.com>
2024-10-18 15:45:24 +0300

    implemented print books, added mising data to books

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-import { books } from './booksData.js';
+import { books } from "./booksData.js";
 
-(() =>{
-           
+(() => {
+  printBooks(books);
+
+  function printBooks(books) {
+    for (const book of books) {
+      console.log(`Categoreis:${book.category}`);
+      for (const individualBook in book.books) {
+        console.log(`Title: ${book.books[individualBook].title}`);
+        console.log(`Author: ${book.books[individualBook].author}`);
+        console.log(`Year: ${book.books[individualBook].year}`);
+        console.log(`ISBM: ${book.books[individualBook].ISBN}`);
+        console.log(`Pages: ${book.books[individualBook].pages}`);
+        console.log(`Quantity: ${book.books[individualBook].quantity}`);
+        console.log(`Price: ${book.books[individualBook].price} \n \n`);
+      }
+      console.log("-----------------------------------------  \n \n");
+    }
+  }
 })();
